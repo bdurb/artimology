0911b96e497648148800c43265b35dcfef79dca8
bdurb <brian@rpmgreenville.com>
2019-03-04 17:52:39 -0800

    setting up initial structure for app

---
+++ src/components/Results.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Results extends Component {
+  state = {  }
+  render() { 
+    return (
+      <div>
+        <h1>another placeholder</h1>
+      </div>
+    );
+  }
+}
+ 
+export default Results;
